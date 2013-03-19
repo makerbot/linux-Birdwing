f67634c9c17e29bf2625df4ad37d9dbc329a24cc
alison leonard <alison.nj.leonard@gmail.com>
2013-03-19 18:07:40 -0400

    spi updates

--- arch/arm/mach-davinci/include/mach/da8xx.h
+++ arch/arm/mach-davinci/include/mach/da8xx.h
@@ -113,6 +113,7 @@ extern struct da8xx_lcdc_platform_data sharp_lcd035q3dg01_pdata;
 extern struct da8xx_lcdc_platform_data sharp_lk043t1dg01_pdata;
 extern struct da8xx_lcdc_platform_data ssd2119_pdata;
 extern struct da8xx_lcdc_spi_platform_data ssd2119_spi_pdata;
+extern struct davinci_spi_platform_data da8xx_spi_pdata[];
 
 extern struct platform_device da8xx_wdt_device;
 
