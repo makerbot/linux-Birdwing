04c2e2a87c1daa8796c0a22a719a9018c61e9ffa
Matt Sterling <matt.sterling@makerbot.com>
2013-11-08 18:17:26 -0500

    Changed some init things, needs work, does not compile at the moment

--- drivers/makerbot/buzzer/buzzer.h
+++ drivers/makerbot/buzzer/buzzer.h
@@ -5,11 +5,26 @@
 
 
 //Structs
+struct buzzer_dev_t{
+	const char	*label;
+	dev_t		devt;
+	struct buzzer_platform_data	*pdev;
+	struct mutex	buzz_lock;
+	u8		*buffer;
+};
+
+struct buzzer_ops{
+	int	(*open)(struct buzzer_dev_t *dev);
+	int	(*read)(struct buzzer_dev_t *dev);
+	void	(*write)(struct buzzer_dev_t *dev);
+	void	(*release)(struct buzzer_dev_t *dev);
+	struct module *owner;
+}
 
 //Functions
-static int buzzer_open(struct);
-static int buzzer_read(struct);
-static void buzzer_write(struct);
-static void buzzer_close(struct);
+//static int buzzer_open(struct);
+//static int buzzer_read(struct);
+//static void buzzer_write(struct);
+//static void buzzer_close(struct);
 
-static void synth(u16, u16, u16, u32, u16);
+//static void synth(u16, u16, u16, u32, u16);
