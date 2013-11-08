51e1d40bb02895abf3e2f19a650e5c4c18be7c64
Matt Sterling <matt.sterling@makerbot.com>
2013-11-08 09:29:00 -0500

    Adding buzzer framework, moved MyFirstDriver

---
+++ drivers/makerbot/buzzer/notes.h
@@ -0,0 +1,109 @@
+/*
+*	Note frequency defines and MIDI note numbers
+*
+*/
+
+//Note to Frequncy
+#define C0	16.35
+#define DB0	17.32
+#define D0	18.35
+#define EB0	19.45
+#define F0	21.83
+#define GB0	23.12
+#define G0	24.50
+#define AB0	25.96
+#define A0	27.50
+#define BB0	29.14
+
+#define C1	32.70
+#define DB1	34.65
+#define D1	36.71
+#define EB1	38.89
+#define E1	41.20
+#define F1	43.65
+#define	GB1	46.25
+#define G1	49.00
+#define	AB1	51.91
+#define A1	55.00
+#define BB1	58.27
+#define B1	61.74
+
+#define C2	65.41
+#define DB2	69.30
+#define D2	73.42
+#define EB2	77.78
+#define F2	82.41
+#define GB2	92.50
+#define G2	98.00
+#define AB2	103.83
+#define A2	110.00
+#define BB2	116.54
+#define B2	123.47
+
+#define C3	130.81
+#define DB3	138.59
+#define D3	146.83
+#define EB3	155.56
+#define E3	164.81
+#define F3	174.61
+#define GB3	185.00
+#define G3	196.00
+#define AB3	207.65
+#define A3	220.00
+#define BB3	233.08
+#define B3	246.94
+
+#define C4	261.63
+#define DB4	277.18
+#define D4	293.66
+#define EB4	311.13
+#define E4	329.63
+#define F4	349.23
+#define GB4	369.99
+#define G4	392.00
+#define AB4	415.30
+#define A4	440.00
+#define BB4	466.16
+#define B4	493.88
+
+#define C5	523.25
+#define DB5	554.37
+#define D5	587.33
+#define EB5	622.25
+#define E5	659.26
+#define F5	698.46
+#define GB5	739.99
+#define G5	830.61
+#define AB5	880.00
+#define A5	932.33
+#define BB5	932.33
+#define B5	987.77
+
+#define C6	1046.50
+#define DB6	1108.73
+#define D6	1174.66
+#define EB6	1244.51
+#define E6	1318.51
+#define F6	1396.91
+#define GB6	1479.98
+#define G6	1567.98
+#define AB6	1661.22
+#define A6	1760.00
+#define BB6	1864.66
+#define B6	1975.53
+
+#define C7	2093.00
+#define DB7	2217.46
+#define D7	2349.32
+#define EB7	2489.02
+#define E7	2637.02
+#define F7	2793.83
+#define GB7	2956.96
+#define G7	3135.96
+#define AB7	3322.44
+#define A7	3520.00
+#define BB7	3729.31
+#define B7	3951.07
+
+#define C8	4186.01
+
