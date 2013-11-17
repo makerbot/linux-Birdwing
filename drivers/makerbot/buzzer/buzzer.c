8535f059256d08eee77491b17458a7583e4680f2
Matt Sterling <matt.sterling@makerbot.com>
2013-11-16 19:54:51 -0500

    Got buzzer device to show up in /dev. Hooray

--- drivers/makerbot/buzzer/buzzer.c
+++ drivers/makerbot/buzzer/buzzer.c
@@ -30,16 +30,16 @@
 //#include "seqeunces.h"			//Sequences from MusicForMakerbots
 
 
-int buzzer_major = BUZZER_MAJOR;
-int buzzer_minor = BUZZER_MINOR;
-int buzzer_count = BUZZER_COUNT;
+//int buzzer_major = BUZZER_MAJOR;
+//int buzzer_minor = BUZZER_MINOR;
+//int buzzer_count = BUZZER_COUNT;
 
-module_param(buzzer_major, int, S_IRUGO);
-module_param(buzzer_minor, int, S_IRUGO);
-module_param(buzzer_count, int, S_IRUGO);
+//module_param(buzzer_major, int, S_IRUGO);
+//module_param(buzzer_minor, int, S_IRUGO);
+//module_param(buzzer_count, int, S_IRUGO);
 
 struct buzzer_dev buzzer;
-
+static struct class *buzzer_class;
 
 static int buzzer_open(struct inode *i, struct file *f){
 	int ret;
@@ -68,7 +68,7 @@ static int buzzer_release(struct inode *i, struct file *f){
 unsigned int poll(struct file *f, struct poll_table_struct *pts){
 	pr_info("Buzzer poll\n");
 	return 0;
-}	
+}
 
 //static void synth(u16 dur, u16 freq_1, u16 freq_2, u32 wave, u16 points){
 	//Start Time
@@ -107,24 +107,34 @@ static const struct file_operations buzzer_fops = {
 	.write = 	buzzer_write,	//write data to the device
 };
 
+static int __init buzzer_create_node(void){
+	if(!buzzer_class){
+		buzzer_class = class_create(THIS_MODULE, "buzzer_class");
+		if(!buzzer_class){
+			pr_err("Buzzer class creation failed.\n");
+			return -1;
+		}
+	}
 
-static void buzzer_setup_cdev(struct buzzer_dev *dev, int index){
-	int ret;
-	int devno = MKDEV(buzzer_major, buzzer_minor+index);
+	buzzer.device = device_create(buzzer_class, NULL, buzzer.devt, NULL, "buzzer_dv");
+	if(!buzzer.device){
+		pr_err("Buzzer device creation failed.\n");
+		return -1;
+	}
 
-	cdev_init(&dev->cdev, &buzzer_fops);
-	dev->cdev.owner = THIS_MODULE;
-	dev->cdev.ops = &buzzer_fops;
-	ret = cdev_add(&dev->cdev, devno, 1);
-	if(ret)
-		pr_err("Buzzer: Error %d adding buzzer%d", ret, index);
+	return 0;
 }
 
+static int __init buzzer_destroy_node(void){
+	if(buzzer.device)
+		device_destroy(buzzer_class, buzzer.devt);
+	class_destroy(buzzer_class);
+
+	return 0;
+}
 
 static int buzzer_probe(struct platform_device *pdev){
 	int ret;
-	//struct buzzer_platform_data *pdata = pdev->dev.platform_data;
-
 	ret = 0;
 	pr_info("Buzzer Probe\n");
 
@@ -140,29 +150,10 @@ static int buzzer_probe(struct platform_device *pdev){
 	//}
 	//memset(buzzer, 0, buzzer_count*sizeof(struct buzzer_dev));
 
-	//TODO make this a loop, only one device for now
-		buzzer.index = 0;	//nothing to index
-		buzzer.freq = 440;	//hz
-		buzzer.duration = 1;	//not sure what this corresponds to, maybe make it msec
-		mutex_init(&buzzer.buzzer_mutex);
-		buzzer_setup_cdev(&buzzer, 0);	//possibly unroll this instead of having another func
-		//platform data?
-
-	//Friend devices? probably don't need this
-	//dev = MKDEV(buzzer_major, buzzer_minor+buzzer_count);
-	//dev += buzzer_p_init(dev);		//??
-	//dev += buzzer_access_init(dev);	//??
-
-	//platform_set_drvdata(pdev, buzzer);
-
 	pr_info("End time: %lu\n", jiffies);
 	pr_info("Ktime: %lld\n", ktime_to_ns(ktime_get()));
 
 	return ret;
-fail:
-	//kfree(buzzer);
-	return ret;
-
 }
 
 static int buzzer_remove(struct platform_device *pdev){
@@ -173,7 +164,7 @@ static int buzzer_remove(struct platform_device *pdev){
 	return ret;
 }
 
-
+//Not sure where this would get called
 static struct platform_driver buzzer_driver = {
 	.driver = {
 		.name = "buzzer",
@@ -183,69 +174,70 @@ static struct platform_driver buzzer_driver = {
 	.remove= buzzer_remove,
 };
 
-static void __exit buzzer_exit(void){
+static int __init buzzer_dev_init(void){
 
-	//dev_t devno;
-	//devno = MKDEV(buzzer_major, buzzer_minor);
-	pr_info("Buzzer Exit\n");
-
-	//platform_driver_unregister(&buzzer_driver);
-	//if(buzzer_devices){
-	//	cdev_del(&buzzer_devices[0].cdev);
-	//	kfree(buzzer_devices);
-	//}
-
-	unregister_chrdev_region(buzzer.dev, buzzer_count);
-
-}
-
-
-static int __init buzzer_init(void){
 	int ret;
-	//dev_t dev = 0;
-	ret = 0;
+
 	pr_info("Buzzer Init\n");
 
-	ret=alloc_chrdev_region(&buzzer.dev, buzzer_minor, buzzer_count, "buzzer");
-	if(ret<0){
-		pr_err("Could not allocate char dev region: %d\n", ret);
+	cdev_init(&buzzer.cdev, &buzzer_fops);
+	buzzer.cdev.owner = THIS_MODULE;
+
+	ret = alloc_chrdev_region(&buzzer.devt, 0, 1, "buzzer_cr");
+	if (ret < 0){
+		pr_err("Chrdev Allocation failed: %d\n", ret);
 		return -1;
 	}
 
-	//if(buzzer_major){
-	//	dev = MKDEV(buzzer_major, buzzer_minor);
-	//	ret = register_chrdev_region(dev, buzzer_count, "buzzer");
-	//} else {
-	//	ret = alloc_chrdev_region(&dev, buzzer_minor, buzzer_count, "buzzer");
-	//	buzzer_major = MAJOR(dev);
-	//}
-	//
-	pr_info("Registered with <%d, %d>\n", MAJOR(buzzer.dev), MINOR(buzzer.dev));
-
-	if(ret <0){
-		pr_warn("Buzzer: can't get major %d\n", buzzer_major);
-		return ret;
+	ret = cdev_add(&buzzer.cdev, buzzer.devt, 1);
+	if(ret){
+		pr_err("Cdev add failed: %d\n", ret);
+		unregister_chrdev_region(buzzer.devt, 1);
+		return -1;
 	}
+	pr_info("Registered with <%d, %d>\n", MAJOR(buzzer.devt), MINOR(buzzer.devt));
 
+	buzzer_create_node();
 
-	//ret = platform_driver_register(&buzzer_driver);
-	//if(ret<0){
-	//	pr_err("Buzzer: can't register platform driver %d\n",ret);
-	//	goto fail;
-	//}
-
-
-	return ret;
-
-//fail:
-//	buzzer_exit();
-//	return ret;
+	return 0;
 }
 
+static void buzzer_dev_cleanup(void){
+	buzzer_destroy_node();
+
+	cdev_del(&buzzer.cdev);
+	unregister_chrdev_region(buzzer.devt, 1);
+}
+
+static int __init buzzer_init(void){
+
+	//FIXME I think this is right
+	mutex_init(&buzzer.mutex);
+
+	if(buzzer_dev_init() !=0){
+		pr_err("Buzzer Device Init failed\n");
+		return -1;
+	}
+
+	//TODO init low level stuff here
+
+	//Assign defaults
+	//TODO need set and get methods for this
+	buzzer.index = 1;	//
+	buzzer.freq = 440;	//Hz
+	buzzer.duration = 500;	//msec? sure
+	buzzer.polywave = 1;	//good enough
+
+	return 0;
+}
+
+static void __exit buzzer_exit(void){
+	buzzer_dev_cleanup();
+}
 module_init(buzzer_init);
 module_exit(buzzer_exit);
 
 MODULE_AUTHOR("Matt Sterling");
 MODULE_DESCRIPTION("Makerbot Buzzer Driver");
 MODULE_LICENSE("GPL");
-MODULE_VERSION("0.2");
+MODULE_VERSION("0.3");
