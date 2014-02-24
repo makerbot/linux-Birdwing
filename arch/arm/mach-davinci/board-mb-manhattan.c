ec1ebe5e107a1ccdb94257a7dd40d6cd49dde1ab
Matt Sterling <matt.sterling@makerbot.com>
2014-02-24 14:00:10 -0500

    Exported the EXP0 and EXP1 pins to use space.

--- arch/arm/mach-davinci/board-mb-manhattan.c
+++ arch/arm/mach-davinci/board-mb-manhattan.c
@@ -322,8 +322,8 @@ static short chamber_heater_pins[] = {
 	DA850_GPIO2_2,	//Chamber heater CS
 	DA850_GPIO3_4,	//Chamber heater MISO
 	DA850_GPIO3_2,	//Chamber heater SOMI
-	DA850_GPIO5_5,	//Chamber heater reserved 0
-	DA850_GPIO5_7,	//Chamber heater reserved 1
+//	DA850_GPIO5_5,	//Chamber heater reserved 0
+//	DA850_GPIO5_7,	//Chamber heater reserved 1
 	-1,
 };
 
@@ -389,6 +389,8 @@ static __init int chamber_heater_init(void){
                 pr_err("ERROR could not request chamber heater RSV1 gpio %d\n", ret);
                 goto free_ch_rsv1;
         }
+	gpio_export(CH_RSV0, 1);
+	gpio_export(CH_RSV1, 1);
 
         return 0;
 free_ch_rsv1:
