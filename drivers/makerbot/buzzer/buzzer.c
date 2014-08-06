575075060f5627915a9a85b4a714789d5daccc50
soundslikesterling <matt.sterling@makerbot.com>
2014-08-05 20:53:05 -0400

    work worky worky worky BW-628

--- drivers/makerbot/buzzer/buzzer.c
+++ drivers/makerbot/buzzer/buzzer.c
@@ -3,7 +3,7 @@
 *
 *	Copyright (c) 2013-2014 Makerbot Industries LLC
 *
-*	Last updated: 3 Aug 2014
+*	Last updated: 5 Aug 2014
 */
 
 #include <linux/init.h>			//init functions
@@ -17,6 +17,8 @@
 #include <linux/device.h>		//class, devices, generic device model
 #include <linux/gpio.h>			//gpio manipulation
 #include <linux/mutex.h>		//mutexalicious
+#include <linux/kthread.h>		//worker threads
+#include <linux/sched.h>		//scheduler
 
 #include <asm/uaccess.h>		//copy to /from user space
 
@@ -27,19 +29,60 @@ struct buzzer_dev buzzer;		//our main man
 static struct class *buzzer_class;	//class seen by the kernel
 
 
-static void buzzer_play_seq(struct work_struct *work){
-
-	pr_debug("Play Seq %d at %d\n", buzzer.song, buzzer.index);
+static void buzzer_synth(struct kthread_work *work){
+	pr_info("synthy synthy\n");
 	return;
 }
 
-static void buzzer_synth(struct work_struct *work){
-	pr_debug("synthy synthy\n");
+int toggle_pin(void *data){
+	pr_info("toggle pin\n");
+	return 0;
+}
+
+
+static void buzzer_play_seq(struct work_struct *work){
+	struct buzzer_dev *b = container_of(
+				container_of(work, struct delayed_work, work),
+				struct buzzer_dev,
+				seq_work);
+	struct sched_param priority = { .sched_priority = MAX_RT_PRIO - 1 };
+
+	pr_info("Play Seq %d at %d\n", b->song, b->index);
+
+	//fi there's still notes in the song, schedule the next one
+	if(sequences[b->song][b->index+6])
+		schedule_delayed_work(&b->seq_work, msecs_to_jiffies(sequences[b->song][b->index]));
+
+	//update the synth params
+	buzzer.duration = sequences[buzzer.song][buzzer.index];
+	buzzer.freq = sequences[buzzer.song][buzzer.index+1];
+	buzzer.polywave = sequences[buzzer.song][buzzer.index+3];	//what's 2 i forget
+	buzzer.npts = sequences[buzzer.song][buzzer.index+4];
+	//set up the worker thread
+	//Needed?
+	//init_kthread_worker(&b->kworker);
+	b->kworker_task = kthread_run(toggle_pin,
+					NULL,//&b->kworker,
+					"synth");
+	if(IS_ERR(b->kworker_task)){
+		pr_err("Cannot create synth task\n");
+		return;
+	}
+	//Needed?
+	//init_kthread_work(&b->synth, buzzer_synth);
+	sched_setscheduler(b->kworker_task, SCHED_FIFO, &priority);
+//	buzzer_synth(sequences[buzzer.song][buzzer.index],		//duration
+//			sequences[buzzer.song][buzzer.index+1],		//Freq
+//			sequences[buzzer.song][buzzer.index+3],		//Wave
+//			sequences[buzzer.song][buzzer.index+4]);	//Npts
+		//call this one last time then end. Nothign else to do.
+	buzzer.index+=6;
+
 	return;
 }
 
 static int buzzer_open(struct inode *i, struct file *f){
-	pr_debug("Buzzer open\n");
+	pr_info("Buzzer open\n");
 	return 0;
 }
 
@@ -49,21 +92,25 @@ static ssize_t buzzer_write(struct file *f, const char __user *buf, size_t len, 
 	char c[len+1];					//buffer for reading characters
 	unsigned int song_num;				//which song do you want?
 
-	mutex_lock(&buzzer.mutex);			//one at a time now boys
+	mutex_lock(&buzzer.buzzer_lock_mutex);			//one at a time now boys
 	ret = 0;
 
-	if(copy_from_user(&c, buf, len))		//read from user space
-		return -EFAULT;				//something went wrong
+	if(copy_from_user(&c, buf, len)){		//read from user space
+		ret = -EFAULT;
+		goto error;
+	}
+
 	c[len] = 0;					//NULL termination for the array
 	if(!(kstrtouint(c,0,&song_num))){		//0 = successful conversion
-		pr_debug("Buzzer write %d\n", song_num);
+		pr_info("Buzzer write %d\n", song_num);
 		if(song_num<SEQ_COUNT){
 			buzzer.song = song_num;		//which tune would you like to hear
 			buzzer.index = 0;		//reset the position in the song
 			schedule_delayed_work(&buzzer.seq_work, msecs_to_jiffies(20)); //toss it on the que
 		} else {
-			pr_err("Invalid sequence number. Valid range is 1-%d\n", SEQ_COUNT);
-			return -ENXIO;
+			pr_err("Invalid sequence number. Valid range is 1-%d\n", (SEQ_COUNT-1));
+			ret= -EBADF;
+			goto error;
 		}
 	} else {					//else kstrtouint threw some error
 		pr_err("Read %s from user\n", c);
@@ -71,28 +118,35 @@ static ssize_t buzzer_write(struct file *f, const char __user *buf, size_t len, 
 			pr_err("Integer parse error\n");
 		if(ret == -ERANGE)
 			pr_err("Overflow Error\n");
+		goto error;
 	}
-	mutex_unlock(&buzzer.mutex);
+	mutex_unlock(&buzzer.buzzer_lock_mutex);
 
 	return len;
+
+error:
+	mutex_unlock(&buzzer.buzzer_lock_mutex);
+	return ret;
 }
 
 static int buzzer_release(struct inode *i, struct file *f){
-	pr_debug("Buzzer release\n");
+	pr_info("Buzzer release\n");
 	return 0;
 }
 
 static long buzzer_ioctl(struct file *file, unsigned int cmd, unsigned long arg){
 
+
+	
 	switch(cmd){
 	case 0:
-		pr_debug("IOCTL: 0\n");
+		pr_info("IOCTL: 0\n");
 		return 0;
 	case 1:
-		pr_debug("IOCTL: 1\n");
+		pr_info("IOCTL: 1\n");
 		return 1;
 	default:
-		pr_debug("IOCTL: wrong\n");
+		pr_info("IOCTL: wrong\n");
 		return -ENOTTY;
 	}
 	return 0;
@@ -109,23 +163,27 @@ static const struct file_operations buzzer_fops = {
 
 static int __init buzzer_dev_config(void){
 	int ret;
-	pr_debug("Buzzer Device Config\n");
+	pr_info("Buzzer Device Config\n");
+
+	buzzer.dev = MKDEV(BUZZER_MAJOR,BUZZER_MINOR);
+	ret = register_chrdev_region(buzzer.dev, 1, "buzzer_cdev");
+	//ret = register_chrdev(BUZZER_MAJOR, "buzzer_cdev", &buzzer_fops);
+	if(ret){
+		pr_err("Char Dev Registration Failed: %d\n", ret);
+		return ret;
+	}
+	pr_info("Registered with <%d, %d>\n", MAJOR(buzzer.dev), MINOR(buzzer.dev));
 
 	cdev_init(&buzzer.cdev, &buzzer_fops);
 	buzzer.cdev.owner = THIS_MODULE;
 
-	//buzzer.dev = MKDEV(BUZZER_MAJOR,BUZZER_MINOR)
-	//maybe this should be 
-	//ret = register_chrdev_region(MKDEV(BUZZER_MAJOR, 0), 1, "buzzer_cdev");
-	//ret = cdev_add(&buzzer.cdev, buzzer.dev, 1);
-	ret = register_chrdev(BUZZER_MAJOR, "buzzer_cdev", &buzzer_fops);
+	ret = cdev_add(&buzzer.cdev, buzzer.dev, 1);
 	if(ret){
-		pr_err("Char Dev Registration Failed: %d\n", ret);
-		return -1;
+		pr_err("Cdev Add Failed: %d\n", ret);
+		return ret;
 	}
-	pr_debug("Registered with <%d, %d>\n", MAJOR(buzzer.dev), MINOR(buzzer.dev));
 
-	pr_debug("Create Buzzer Class\n");
+	pr_info("Create Buzzer Class\n");
 	if(!buzzer_class){
 		buzzer_class= class_create(THIS_MODULE, "buzzer_class");
 		if(!buzzer_class){
@@ -133,14 +191,15 @@ static int __init buzzer_dev_config(void){
 			return -2;
 		}
 	}
-	pr_debug("Create buzzer device\n");
+
+	pr_info("Create buzzer device\n");
 	buzzer.device = device_create(buzzer_class, NULL, buzzer.dev, NULL, "buzzer");
 	if(!buzzer.device){
 		pr_err("Buzzer device creation failed\n");
 		return -3;
 	}
 
-	pr_debug("Request buzzer GPIO\n");
+	pr_info("Request buzzer GPIO\n");
 	ret = gpio_request_one(BUZZER_OUT, GPIOF_OUT_INIT_LOW, "buzzer_gpio");
 	if(ret){
 		pr_err("GPIO Request failed: %d\n", ret);
@@ -156,9 +215,10 @@ static int __init buzzer_dev_config(void){
 static int __init buzzer_init(void){
 	int ret;
 	pr_info("Buzzer init\n");
+	pr_info("Buzzer v 0.7\n");
 
 	//TODO init mutex, spinlock, workqueues
-	mutex_init(&buzzer.mutex);
+	mutex_init(&buzzer.buzzer_lock_mutex);
 	INIT_DELAYED_WORK(&buzzer.seq_work, buzzer_play_seq);
 	ret = buzzer_dev_config();
 	if(ret!=0){
@@ -166,22 +226,26 @@ static int __init buzzer_init(void){
 		return -1;
 	}
 
-	//TODO init struct values: index, freq, duration, etc
-
+	buzzer.index = 0;
+	buzzer.freq = 440;
+	buzzer.duration = 2000;
+	buzzer.polywave = 1;
+	buzzer.npts = 2;
 	return 0;
 }
 
 static void __exit buzzer_exit(void){
-	pr_debug("Buzzer exit\n");
+	pr_info("Buzzer exit\n");
 
-	pr_debug("Destroy Buzzer Device\n");
-	if(buzzer.device)
-		device_destroy(buzzer_class, buzzer.dev);
-	pr_debug("Destroy Buzzer Class\n");
+	pr_info("Free GPIO\n");
+	gpio_free(BUZZER_OUT);
+	pr_info("Destroy Buzzer Device\n");
+	device_destroy(buzzer_class, buzzer.dev);
+	pr_info("Destroy Buzzer Class\n");
 	class_destroy(buzzer_class);
-	pr_debug("Delete cdev\n");
+	pr_info("Delete cdev\n");
 	cdev_del(&buzzer.cdev);
-	pr_debug("Unregister region\n");
+	pr_info("Unregister region\n");
 	unregister_chrdev_region(buzzer.dev, 1);
 }
 
--- drivers/makerbot/buzzer/buzzer.h
+++ drivers/makerbot/buzzer/buzzer.h
@@ -8,6 +8,8 @@
 #include <linux/workqueue.h>
 #include <linux/cdev.h>
 #include <linux/mutex.h>
+#include <linux/kthread.h>
+#include <linux/sched.h>
 //Defines
 #ifndef BUZZER_MAJOR
 #define BUZZER_MAJOR 14		//TODO change this to 14 (sound) later
@@ -40,20 +42,27 @@ struct buzzer_dev{
 	dev_t dev;				//device type, this is actually just a number
 	//TODO add class here?
 	struct cdev cdev;			//character driver
-	struct mutex mutex;			//mutual exclusion semaphore
+	struct mutex buzzer_lock_mutex;		//mutual exclusion semaphore
 	//spinlock_t spin_lock;			//spinlock
 	struct device *device;			//hmm, what is this?
 	//struct buzzer_platform_data *pdata;	//platform data
-	//Buzzer specific 
-	unsigned int index;			//index to be used with frequency
+	//Buzzer specific
+	//synth params
 	unsigned int freq;			//current frequency (pitch)
 	unsigned int duration;			//duration of the note (time)
 	unsigned int polywave;			//"polywave", this changes the timber of the note
+	unsigned int npts;			//npts works with polywave to determine timbre
+	//work structs / threads
 	struct delayed_work seq_work;		//holds the scheduling work que
-	struct delayed_work s_work;		//synth work
+	struct delayed_work synth_work;		//synth work
+	bool synth_running;
+	struct kthread_worker kworker;
+	struct task_struct *kworker_task;
+	struct kthread_work synth;
+	struct sched_param priority;
+	//Song/sequence params
+	unsigned int index;			//note number in the sequence
 	unsigned int song;			//current song to play
-	unsigned int countdown;
-	unsigned int value;
 	//array of notes (pitches)
 	//array of tones (timbres)
 };
